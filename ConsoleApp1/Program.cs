363b350b7db28222b2ef0250ca2525e29d704f6a
mine <ch199108@gmail.com>
2018-08-23 17:07:40 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            using (StreamWriter sw = new StreamWriter("PublicKey.xml"))//产生公匙
+            {
+                sw.WriteLine(rsa.ToXmlString(false));
+            }
+            using (StreamWriter sw = new StreamWriter("PrivateKey.xml"))//产生私匙（也包含私匙）
+            {
+                sw.WriteLine(rsa.ToXmlString(true));
+            }
+
+            var re = Encryption("aaa");
+
+
+            var text = "";
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] palindata = Encoding.Default.GetBytes(text);//将要加密的字符串转换为字节数组
+            byte[] encryptdata = md5.ComputeHash(palindata);//将字符串加密后也转换为字符数组
+            var resultMd5 = Convert.ToBase64String(encryptdata);//将加密后的字节数组转换为加密字符串
+
+
+
+
+
+
+            var bsStr = UnBase64String("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9");
+            Console.WriteLine(bsStr);
+            var enStr = ToBase64String(bsStr);
+            Console.WriteLine(enStr);
+
+            var result = MyHmac.CreateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ", "123");
+            Console.WriteLine(result);
+
+
+            Console.ReadKey();
+        }
+
+        //加密
+        private static string Encryption(string express)
+        {
+            CspParameters param = new CspParameters();
+            param.KeyContainerName = "oa_erp_dowork";//密匙容器的名称，保持加密解密一致才能解密成功
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(param))
+            {
+                byte[] plaindata = Encoding.Default.GetBytes(express);//将要加密的字符串转换为字节数组
+                byte[] encryptdata = rsa.Encrypt(plaindata, false);//将加密后的字节数据转换为新的加密字节数组
+                return Convert.ToBase64String(encryptdata);//将加密后的字节数组转换为字符串
+            }
+        }
+
+        //解密
+        private static string Decrypt(string ciphertext)
+        {
+            CspParameters param = new CspParameters();
+            param.KeyContainerName = "oa_erp_dowork";
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(param))
+            {
+                byte[] encryptdata = Convert.FromBase64String(ciphertext);
+                byte[] decryptdata = rsa.Decrypt(encryptdata, false);
+                return Encoding.Default.GetString(decryptdata);
+            }
+        }
+        public static string UnBase64String(string value)
+        {
+            if (value == null || value == "")
+            {
+                return "";
+            }
+            byte[] bytes = Convert.FromBase64String(value);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public static string ToBase64String(string value)
+        {
+            if (value == null || value == "")
+            {
+                return "";
+            }
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+
+
+    public class MyHmac
+    {
+        public static string CreateToken(string message, string secret)
+        {
+            secret = secret ?? "";
+            var encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = encoding.GetBytes(secret);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hashmessage);
+            }
+        }
+    }
+
+}
